8c92a9133bc8daf91f061d31a5cac7febe55e6a2
Finn Wohner <finnjwohner@gmail.com>
2024-04-08 17:56:05 +0100

    Configured cors options

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'https://www.edinburghtrafficsim.com',
+    optionsSuccessStatus: 200
+}
 
 const roadLinkPath = path.join(__dirname, './geoJson/roadLinkLatLong.json');
 const roadNodePath = path.join(__dirname, './geoJson/roadNodeLatLong.json');
@@ -16,7 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
-app.get('/geoJson', (req, res) => {
+app.get('/geoJson', cors(corsOptions), (req, res) => {
     res.json({
         roadLink: roadLinkJson,
         roadNode: roadNodeJson,
